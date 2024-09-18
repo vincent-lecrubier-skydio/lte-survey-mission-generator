286acece8f90aa2fa37c661efc67a39c93bf5b3f
Vincent Lecrubier <94416008+vincent-lecrubier-skydio@users.noreply.github.com>
2024-09-18 01:30:18 +0000

    Added altitude, return settings, download zip

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,8 @@ import uuid
 from datetime import datetime
 import httpx
 import asyncio
+import io
+import zipfile
 
 import numpy as np
 from shapely.geometry import LineString, Polygon
@@ -121,11 +123,15 @@ def split_polygon_into_squares(polygon, size):
     return squares
 
 
-def generate_mission(linestring, index, altitude, name_prefix="LTE Scan"):
+def generate_mission(linestring, index, altitude, name_template, rtx_height, rtx_speed, rtx_wait):
     with open("mission.proto.json", "r", encoding="utf8") as mission_proto_file:
         mission_proto = json.load(mission_proto_file)
-        mission_proto["displayName"] = f"{name_prefix} - {index} - {datetime.now().isoformat()}"
+        mission_proto["displayName"] = name_template.format(
+            index=index, date=datetime.now().isoformat())
         mission_proto["templateUuid"] = str(uuid.uuid4())
+        mission_proto["rtxSettings"]["minimumHeight"] = rtx_height
+        mission_proto["rtxSettings"]["speed"] = rtx_speed
+        mission_proto["rtxSettings"]["waitTime"] = rtx_wait
         mission_proto["actions"][0]["actionUuid"] = str(uuid.uuid4())
         mission_proto['actions'][0]['args']['sequence']["actions"] = [
             {
@@ -170,7 +176,7 @@ def generate_mission(linestring, index, altitude, name_prefix="LTE Scan"):
                                             },
                                             "z": {
                                                 "frame": 5,
-                                                "value": 61.0  # m = 200ft
+                                                "value": altitude  # m = 200ft
                                             },
                                             "heading": {
                                                 "value": 1.5707963267948966,
@@ -225,42 +231,64 @@ def generate_mission(linestring, index, altitude, name_prefix="LTE Scan"):
 mission_file = st.file_uploader(
     "Upload a geojson file (from geojson.io) with one polygon of the entire city boundary that you want to scan", type=["json", "geojson"])
 
-passes = st.multiselect(
-    "Which scan passes do you want to fly?",
-    ["North-South", "East-West"],
-    ["North-South"],
-)
+name_template = st.text_input(
+    "Mission name template:", value="LTE Scan - {index} - {date}")
 
-st.markdown(
-    "We split that polygon into many separate squares, each representing a mission to fly")
-square_size = st.number_input(
-    "Size of mission squares, in meter:", min_value=100, value=800)
-st.markdown(
-    f"Square size = {square_size:.0f}m = {square_size*3.28084:.0f}ft = {square_size*1.09361:.0f}yd = {square_size/1609:.2f} Miles = {square_size/1852:.2f} Nautical Miles")
-st.markdown("For each square, we generate a lawnmower pattern to scan the area with a given spacing between passes")
-spacing = st.number_input(
-    "Spacing between passes in meters:", min_value=10, value=50)
-st.markdown(
-    f"Spacing = {spacing:.0f}m = {spacing*3.28084:.0f}ft = {spacing*1.09361:.0f}yd = {spacing/1609:.2f} Miles = {spacing/1852:.2f} Nautical Miles")
+with st.expander("Mission Planning Parameters"):
+    passes = st.multiselect(
+        "Which scan passes do you want to fly?",
+        ["North-South", "East-West"],
+        ["North-South"],
+    )
 
-speed = st.number_input(
-    "Flight speed in meters per second:", min_value=0, value=14)
-st.markdown(
-    f"Flight speed = {speed:.0f}m/s = {speed*3.6:.1f}km/h = {speed*3600/1609:.1f}Mph = {speed*3600/1852:.1f}kts")
+    st.markdown(
+        "We split that polygon into many separate squares, each representing a mission to fly")
+    square_size = st.number_input(
+        "Size of mission squares, in meter:", min_value=100, value=600)
+    st.markdown(
+        f"Square size = {square_size:.0f}m = {square_size*3.28084:.0f}ft = {square_size*1.09361:.0f}yd = {square_size/1609:.2f} Miles = {square_size/1852:.2f} Nautical Miles")
+    st.markdown(
+        "For each square, we generate a lawnmower pattern to scan the area with a given spacing between passes")
+    spacing = st.number_input(
+        "Spacing between passes in meters:", min_value=10, value=50)
+    st.markdown(
+        f"Spacing = {spacing:.0f}m = {spacing*3.28084:.0f}ft = {spacing*1.09361:.0f}yd = {spacing/1609:.2f} Miles = {spacing/1852:.2f} Nautical Miles")
 
-altitude = st.number_input(
-    "Flight altitude in meters:", min_value=10, value=61)
-st.markdown(
-    f"Altitude = {altitude:.0f}m = {altitude*3.28084:.0f}ft = {altitude*1.09361:.0f}yd = {altitude/1609:.2f} Miles = {altitude/1852:.2f} Nautical Miles")
+    speed = st.number_input(
+        "Flight speed in meters per second:", min_value=0, value=14)
+    st.markdown(
+        f"Flight speed = {speed:.0f}m/s = {speed*3.6:.1f}km/h = {speed*3600/1609:.1f}Mph = {speed*3600/1852:.1f}kts")
 
-cost_fixed = st.number_input(
-    "Fixed base cost for the whole program:", min_value=0, value=5000)
+    altitude = st.number_input(
+        "Flight altitude in meters:", min_value=10, value=61)
+    st.markdown(
+        f"Altitude = {altitude:.0f}m = {altitude*3.28084:.0f}ft = {altitude*1.09361:.0f}yd = {altitude/1609:.2f} Miles = {altitude/1852:.2f} Nautical Miles")
 
-cost_per_flight = st.number_input(
-    "Fixed cost per flight mission:", min_value=0, value=100)
+with st.expander("Return Settings"):
+    rtx_height = st.number_input(
+        "Return Height in meters:", min_value=10, value=61)
+    st.markdown(
+        f"Return Height = {rtx_height:.0f}m = {rtx_height*3.28084:.0f}ft = {rtx_height*1.09361:.0f}yd = {rtx_height/1609:.2f} Miles = {rtx_height/1852:.2f} Nautical Miles")
 
-cost_per_flight_hour = st.number_input(
-    "Variable cost per flight hour:", min_value=0, value=50)
+    rtx_speed = st.number_input(
+        "Return Speed in meters per second:", min_value=0, value=14)
+    st.markdown(
+        f"Return Speed = {rtx_speed:.0f}m/s = {rtx_speed*3.6:.1f}km/h = {rtx_speed*3600/1609:.1f}Mph = {rtx_speed*3600/1852:.1f}kts")
+
+    rtx_wait = st.number_input(
+        "Wait Before Return on Lost Connection:", min_value=0, value=30)
+    st.markdown(
+        f"Wait Before Return on Lost Connection = {rtx_wait:.0f}s = {rtx_wait/60:.0f}min{rtx_wait%60:.0f}s")
+
+with st.expander("Cost Estimation Parameters"):
+    cost_fixed = st.number_input(
+        "Fixed base cost for the whole program:", min_value=0, value=5000)
+
+    cost_per_flight = st.number_input(
+        "Fixed cost per flight mission:", min_value=0, value=100)
+
+    cost_per_flight_hour = st.number_input(
+        "Variable cost per flight hour:", min_value=0, value=50)
 
 mission_length = (  # horizontal passes
     square_size *  # length of each leg
@@ -289,14 +317,26 @@ if mission_file is not None:
 
     squares = split_polygon_into_squares(input_polygon, size=square_size)
 
+    if mission_duration > 1800:
+        st.markdown(
+            "🔴 :red[**Missions are longer than 30 minutes! They probably are not going to be flyable**]")
+    elif mission_duration > 1500:
+        st.markdown(
+            "🟡 :orange[**Missions are in the 25-30minutes range. This is ok but leaves little margin. Some mission might not finish completely in one battery.**]")
+    elif mission_duration > 900:
+        st.markdown(
+            "🟢 :green[**Missions are in the 15-25minutes range. This is ideal**]")
+    elif mission_duration > 600:
+        st.markdown(
+            "🟡 :orange[**Missions are in the 10-15minutes range. This is ok but a little bit on the short range. Try making larger missions**]")
+    else:
+        st.markdown(
+            "🔴 :red[**Missions are shorter than 10 minutes! This is very short and will add operational overhead, try making larger missions.**]")
     st.markdown(f"Total number of missions to fly: {len(squares)}")
     st.markdown(
         f"Length of each mission = {mission_length:.0f}m = {mission_length*3.28084:.0f}ft = {mission_length*1.09361:.0f}yd = {mission_length/1609:.2f} Miles = {mission_length/1852:.2f} Nautical Miles")
     st.markdown(
         f"Duration of each mission = {mission_duration:.0f}s = {mission_duration/60:.0f}min")
-    if mission_duration > 1800:
-        st.markdown(
-            ":red[**Mission are longer than 30 minutes! They probably are not going to be flyable**]")
     st.markdown(
         f"Total flight hours (Conservative) = {len(squares)*mission_duration/3600:.0f}h")
     st.markdown(
@@ -330,7 +370,7 @@ if mission_file is not None:
 
         if st.button("Generate File"):
             mission_json_data = generate_mission(
-                lawnmower_pattern, input_square_index, altitude)
+                lawnmower_pattern, input_square_index, altitude, name_template, rtx_height, rtx_speed, rtx_wait)
 
             st.download_button(
                 label="Download mission.json",
@@ -339,11 +379,35 @@ if mission_file is not None:
                 mime="application/json",
             )
 
+    with st.expander("Get all missions json"):
+
+        if st.button("Generate zip file of all missions"):
+            # Create an in-memory bytes buffer to hold the ZIP file
+            zip_buffer = io.BytesIO()
+            with zipfile.ZipFile(zip_buffer, "a", zipfile.ZIP_DEFLATED, False) as zip_file:
+                for i, square in enumerate(squares, start=1):
+                    lawnmower_pattern = generate_lawnmower_pattern(
+                        square, spacing=spacing, passes=passes)
+                    if lawnmower_pattern.is_empty:
+                        continue
+                    mission_json_data = generate_mission(
+                        lawnmower_pattern, i, altitude, name_template, rtx_height, rtx_speed, rtx_wait)
+                    # Write the mission JSON data into the ZIP file
+                    filename = f"mission_{i}.json"
+                    zip_file.writestr(filename, mission_json_data)
+
+            # Seek to the beginning of the BytesIO buffer
+            zip_buffer.seek(0)
+            # Provide the ZIP file for download
+            st.download_button(
+                label="Download zip file",
+                data=zip_buffer,
+                file_name="missions.zip",
+                mime="application/zip"
+            )
+
     with st.expander("Upload all missions to Skydio Cloud"):
         api_key = st.text_input("Enter your API key", type="password")
-
-        name_prefix = st.text_input(
-            "Enter a prefix for the mission names", value="LTE Scan")
 
         async def upload_mission(session, request_url, headers, mission_json_data, i):
             response = await session.post(request_url, headers=headers, data=mission_json_data)
@@ -368,7 +432,7 @@ if mission_file is not None:
                     if lawnmower_pattern.is_empty:
                         continue
                     mission_json_data = generate_mission(
-                        lawnmower_pattern, i, altitude, name_prefix)
+                        lawnmower_pattern, i, altitude, name_template, rtx_height, rtx_speed, rtx_wait)
                     tasks.append(upload_mission(session, request_url,
                                                 headers, mission_json_data, i))
 
